752abd67b6e64a1ce801632aaaa31ae6731dfaa3
amoon05 <aaronmoonn@gmail.com>
2024-02-16 06:48:32 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
 
 function App() {
   const [pokemonName, setPokemonName] = useState("")
   const [pokemonChosen, setPokemonChosen] = useState(false)
+  const [pokemonData, setPokemonData] = useState(null)
+  const [pokemonDataList, setPokemonDataList] = useState([])
   const [pokemon, setPokemon] = useState({
     name: "",
     species: "",
@@ -25,7 +27,6 @@ function App() {
         console.log(response)
         setPokemon({
           name: pokemonName,
-          species: response.data.species.name,
           picture: response.data.sprites.front_default,
           hp: response.data.stats[0].base_stat,
           attack: response.data.stats[1].base_stat,
@@ -38,6 +39,30 @@ function App() {
         setPokemonChosen(true)
       })
   }
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        const response = await Axios.get(`https://pokeapi.co/api/v2/pokemon?limit=1302`)
+        console.log(response)
+        const pokemonList = response.data.results;
+        const fetchedPokemonDataList = [];
+        for(let i = 0; i < pokemonList.length; i++) {
+          const detailResponse = await Axios.get(pokemonList[i].url)
+          fetchedPokemonDataList.push({
+            name: pokemonList[i].name,
+            picture: detailResponse.data.sprites.front_default,
+          })
+        }
+        
+        setPokemonDataList(fetchedPokemonDataList)
+      } catch (error) {
+        console.log("error", error)
+      }
+    }
+
+    fetchPokemonData();
+  }, [])
   return(
     <div className={styles.app}>
       <section className={styles.header}>
@@ -67,14 +92,13 @@ function App() {
           </div>
         </div>
       </section>
-      <section className={styles.display}>
+      <section className={styles.displaySearch}>
         <div className={styles.pokemonInfoContainer}>{!pokemonChosen ? (
         <h1>Please choose a Pokemon</h1>
         ) : (
           <>
             <h1>Name: {pokemon.name}</h1>
             <img src={pokemon.picture}/>
-            <h2>Species: {pokemon.species}</h2>
             <h2>Type: {pokemon.type}</h2>
             <h2>Hp Stats: {pokemon.hp}</h2>
             <h2>Attack Stats: {pokemon.attack}</h2>
@@ -83,6 +107,21 @@ function App() {
             <h2>Special Defense Stats: {pokemon.specialDefense}</h2>
             <h2>Sped Stats: {pokemon.speed}</h2>
           </>
+        )}</div>
+      </section>
+      <section className={styles.displayList}>
+        <div className={styles.container}>
+          {!pokemonDataList.length ? (
+          <p>Loading...</p>
+        ) : (
+          <div className={styles.pokemonListContainer}>
+            {pokemonDataList.map((pokemonData) => (
+              <div key={pokemonData.name} className={styles.pokemonInfoContainer}>
+                  <h1>{pokemonData.name}</h1>
+                  <img src={pokemonData.picture}/>
+              </div>
+            ))}
+          </div>
         )}</div>
       </section>
     </div>
